b1dfdc555ba1d1a2504d8aa45cd9a1ae83f2cfdd
Raihan <ec16277@qmul.ac.uk>
2017-12-31 16:25:21 +0000

    Converted to ES6 class syntax

--- Webhook.js
+++ Webhook.js
@@ -1,25 +1,53 @@
 const request = require("request");
-const endpoint = "https://discordapp.com/api/";
-module.exports = class WebhookClass {
+class Webhook {
+
     constructor(uri) {
         this.url = uri;
-        this.id = "";
-        this.token = "";
 
+        // done with a request to "test" whether a valid webhook before continuing
         request(this.url, function (error, response, body) {
-            if (error) {
-                console.log("Could not get webhook info: "+error);
-                return;
-            }
+            console.log(body);
+
+            if (error) console.log("Could not get webhook info: " + error);
             try {
                 this.rawData = JSON.parse(body);
                 this.token = this.rawData.token;
                 this.id = this.rawData.id;
                 this.webhookName = this.rawData.name;
-
-                console.log("Created webhook called " + this.webhookName + " with token: "+this.token+" and ID: "+this.id);
+                console.log("Created webhook called " + this.webhookName + " with token: " + this.token + " and ID: " + this.id);
             }
-            catch (err) { console.log("Could not create webhook: "+err.stack) }
+            catch (err) {
+                console.log("Could not create webhook: " + err.stack)
+            }
         });
     }
-};
+
+    sendMessage(title, msg) {
+        try {
+            var d = {
+                "attachments": [{
+                    "color": "#f797ff",
+                    "fields": [{
+                        "title": title,
+                        "value": msg
+                    }],
+                    "ts": new Date() / 1000
+                }]
+            };
+            request({
+                url:this.url + "/slack",
+                method:"POST",
+                body:d,
+                json:true
+            }, (e,r,b) => {
+                if (e) console.log(e);
+                //if (r) console.log(r);
+                if (b) console.log(b);
+            });
+        } catch (err) {
+            console.log("Error: " + err.stack)
+        }
+    };
+}
+
+module.exports = Webhook;
